8726e7fa7dded4abe4d38eb494c59e6ce493e955
Rodrigo Lopes <rlopeslameira@gmail.com>
2020-06-01 16:14:59 -0300

    ajust login screen

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,31 +1,38 @@
-import  { all, takeLatest, call, put } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import history from '../../../services/history';
 import { signInSuccess, signFailure } from './actions';
 
-export function* signIn({payload}){
+export function* signIn({ payload }) {
 
-  try{
-    const {email, password } = payload;
+  try {
+    const { email, password } = payload;
 
     const response = yield call(api.post, 'sessions', {
       email, password
-    }); 
+    });
 
-    const {token, user} = response.data;
+    if (response.data.error) {
+      toast.error(response.data.error);
+      yield put(signFailure());
+    } else {
 
-    if (!user.provider){
-      toast.error('Usuário não é prestador.')
-      return;
+      const { token, user } = response.data;
+
+      if (!user.provider) {
+        toast.error('Usuário não é prestador.')
+        return;
+      }
+
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+
+      yield put(signInSuccess(token, user));
+
+      history.push('/dashboard');
     }
 
-    api.defaults.headers.Authorization = `Bearer ${token}`; 
-
-    yield put(signInSuccess(token, user));
-    
-    history.push('/dashboard');
   } catch (err) {
     toast.error('Falha na autenticação, verifique seus dados.')
     yield put(signFailure());
@@ -33,14 +40,14 @@ export function* signIn({payload}){
 
 }
 
-export function* signUp({payload}){
+export function* signUp({ payload }) {
 
-  try{
-    const {name, email, password } = payload;
+  try {
+    const { name, email, password } = payload;
 
     yield call(api.post, 'users', {
       name, email, password, provider: true,
-    }); 
+    });
 
     toast.success('Cadastro realizado com sucesso!')
 
@@ -52,18 +59,18 @@ export function* signUp({payload}){
 
 }
 
-export function setToken({payload}){
+export function setToken({ payload }) {
   if (!payload) return;
 
-  const {token} = payload.auth;
+  const { token } = payload.auth;
 
   if (token)
-    api.defaults.headers.Authorization = `Bearer ${token}`; 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
 
-export function signOut(){
-    history.push('/');
+export function signOut() {
+  history.push('/');
 }
 
 export default all([
